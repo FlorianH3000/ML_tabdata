513492e4502a36e0a95259b7380fe54151711d3d
FlorianH3000 <67954588+FlorianH3000@users.noreply.github.com>
2021-12-13 11:20:57 +0100

    Update data_import.py

--- data_import.py
+++ data_import.py
@@ -11,7 +11,7 @@ from sklearn.preprocessing import LabelEncoder
 label_encoder = LabelEncoder()
 
 
-path = r'/home/florian/Desktop/code/Risikoadjustierung/Hüfte final_EPRD_endocert.xlsx'
+path = r'xxx.xlsx'
 
 data_og = pd.read_excel(path)
 data = data_og.to_numpy().transpose()
@@ -49,15 +49,15 @@ data_input = np.stack(
     data3,
     data4,
     data5,
-    data6, # operateur
-    data7, # Alter
+    data6,
+    data7,
     data8,
-    data9, # Gewicht
+    data9,
     data10,
     data11,
     data12,
-    data13, # Voroperation
-    data14, # Wechsel/ Reoperationsgrund
+    data13,
+    data14,
     data15,
     )).transpose()
 
@@ -65,79 +65,8 @@ data_input = np.stack(
 
 # data_input = data[1:15].transpose()
 
-
-### data label ###
-data_label = data[37]
-
+### data label - adjust xxx ###
+data_label = data[xxx]
 
 
 
-
-
-
-
-
-# # =============================================================================
-# # knee
-# # =============================================================================
-
-# label_encoder = LabelEncoder()
-
-
-# path = r'/home/florian/Desktop/code/Risikoadjustierung/knee primary.xlsx'
-
-# data_og = pd.read_excel(path)
-# data = data_og.to_numpy().transpose()
-
-
-# """ data sample 1 """
-# # data_1 = data[0]
-
-# """ data sample 2 """
-# # data_2 = data[1]
-
-# """ data input """
-
-# data1 = label_encoder.fit_transform(data[1])
-# data2 = label_encoder.fit_transform(data[2])
-# data3 = label_encoder.fit_transform(data[3])
-# data4 = label_encoder.fit_transform(data[4])
-# data5 = label_encoder.fit_transform(data[5])
-# data6 = label_encoder.fit_transform(data[6])
-# data7 = label_encoder.fit_transform(data[7])
-# data8 = label_encoder.fit_transform(data[8])
-# data9 = label_encoder.fit_transform(data[9])
-# data10 = label_encoder.fit_transform(data[10])
-# data11 = label_encoder.fit_transform(data[11])
-# data12 = label_encoder.fit_transform(data[12])
-# data13 = label_encoder.fit_transform(data[13])
-# data14 = label_encoder.fit_transform(data[14])
-# data15 = label_encoder.fit_transform(data[15])
-
-
-# data_input = np.stack(
-#     (
-#     data1,
-#     data2,
-#     data3,
-#     data4,
-#     data5,
-#     data6, # operateur
-#     data7, # Alter
-#     data8,
-#     data9, # Gewicht
-#     data10,
-#     data11,
-#     data12,
-#     data13, # Voroperation
-#     data14, # Wechsel/ Reoperationsgrund
-#     data15,
-#     )).transpose()
-
-
-
-# # data_input = data[1:15].transpose()
-
-
-# """ data label """
-# data_label = data[36]
